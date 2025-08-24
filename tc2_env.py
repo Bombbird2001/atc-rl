6398e10c923b3db1a561c0df66f488c7638cef61
Shang Yi Ku <kushangyi2001@gmail.com>
2025-08-25 01:26:43 +0800

    Working training setup

--- tc2_env.py
+++ tc2_env.py
@@ -4,11 +4,14 @@ import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 import mmap
+import random
+import struct
 import win32event
 
 
-# 28 bytes shared region: [proceed flag(1 byte)] [action(3 bytes)] [reward(1 byte)] [terminated(1 byte)] [empty padding(2 bytes)] [state(20 bytes (5x floats))]
-FILE_SIZE = 28
+# 32 bytes shared region: [proceed flag(1 byte)] [action(3 bytes)] [reward(4 bytes (1 float))] [terminated(1 byte)] [empty padding(2 bytes)] [state(20 bytes (5x floats))]
+FILE_SIZE = 32
+STRUCT_FORMAT = "bbbbf?xxxfffff"
 
 # Create anonymous memory-mapped file with a local name
 mm = mmap.mmap(-1, FILE_SIZE, tagname="Local\\ATCRLSharedMem")
@@ -17,13 +20,16 @@ mm = mmap.mmap(-1, FILE_SIZE, tagname="Local\\ATCRLSharedMem")
 reset_sim = win32event.CreateEvent(None, False, False, "Local\\ATCRLResetEvent")
 action_ready = win32event.CreateEvent(None, False, False, "Local\\ATCRLActionReadyEvent")
 action_done = win32event.CreateEvent(None, False, False, "Local\\ATCRLActionDoneEvent")
+reset_after_step = win32event.CreateEvent(None, False, False, "Local\\ATCRLResetAfterEvent")
 
 
 class TC2Env(gym.Env):
     # metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-    def __init__(self, render_mode=None):
+    def __init__(self, is_eval=False, render_mode=None):
         super().__init__()
+
+        self.is_eval = is_eval
 
         # Actions[0] = [steps of 5 degree from 0-355]
         # Actions[1] = [steps of 1000 feet from min to max altitude - 2000 to FL150 for Singapore]
@@ -39,6 +45,7 @@ class TC2Env(gym.Env):
         self.state_multiplier = np.array([4000, 4000, 15000 - 2000, 600, 360])
         self.state_adder = np.array([-2000, -2000, 2000, 0, 0])
 
+        self.episode = 0
         self.steps = 0
         self.max_steps = 400
         self.render_mode = render_mode
@@ -52,31 +59,60 @@ class TC2Env(gym.Env):
         # Send reset signal to simulator
         win32event.SetEvent(reset_sim)
         # Wait for simulator to signal ready for next action
+        print(f"Waiting for action ready after reset: episode {self.episode}")
         win32event.WaitForSingleObject(action_ready, win32event.INFINITE)
 
-        # TODO Get state from shared memory
-        raise NotImplementedError()
+        # Get state from shared memory
+        mm.seek(12)
+        bytes_read = mm.read(20)
+        values = struct.unpack("fffff", bytes_read)
+        obs = self.normalize_sim_state(np.array(values))
+        # print(obs)
 
         info = {}
+        self.episode += 1
         return obs, info
 
     def step(self, action):
         # Validate that simulator is ready to accept action (proceed flag)
         mm.seek(0)
-        proceed_flag = mm.read_byte()
+        values = struct.unpack(STRUCT_FORMAT, mm.read(FILE_SIZE))
+        # print(int(time.time() * 1000), values)
+        proceed_flag = values[0]
         if proceed_flag != 1:
             raise InvalidStateError("Proceed flag must be 1")
-        mm.write_byte(0) # Reset flag
 
-        # TODO Write action to shared memory
+        # Write action to shared memory and signal
+        mm.seek(0)
+        mm.write(struct.pack("bbbb", 1, action[0], action[1], action[2]))
+
+        # Set the reset request flag before signalling action done
+        # The next time the game loop finishes simulating 300 frames, it will stop the update till reset() is called here
+        self.steps += 1
+        truncated = self.steps >= self.max_steps
+        if (truncated and not self.is_eval) or values[5]:
+            # print(f"Truncating={truncated}, Terminating={values[5]}")
+            win32event.SetEvent(reset_after_step)
+
+        # print(int(time.time() * 1000), "Signalled action done")
+        win32event.SetEvent(action_done)
+
+        # print("Waiting for action ready")
 
         # Wait till simulator finished simulating 300 frames (action_ready event)
-        # TODO Then read state, reward, terminated, truncated from shared memory
         win32event.WaitForSingleObject(action_ready, win32event.INFINITE)
-        raise NotImplementedError()
+        # print(int(time.time() * 1000), "Action ready")
+
+        # Read state, reward, terminated, truncated from shared memory
+        mm.seek(0)
+        values = struct.unpack(STRUCT_FORMAT, mm.read(FILE_SIZE))
+        obs = self.normalize_sim_state(np.array(values[6:11]))
+        reward = values[4]
+        terminated = values[5]
 
         info = {}
 
+        # print(obs, reward, terminated, truncated)
         return obs, reward, terminated, truncated, info
 
     def render(self):
@@ -86,7 +122,8 @@ class TC2Env(gym.Env):
         pass
 
 
-while True:
-    flag_val = int(input("Input an integer to set: "))
-    mm.seek(0)
-    mm.write(flag_val.to_bytes())
+if __name__ == "__main__":
+    env = TC2Env()
+    env.reset()
+    while True:
+        env.step(np.array([random.randint(0, 360 // 5 - 1), random.randint(0, 13), random.randint(0, 9)]))
--- tc2_ppo.py
+++ tc2_ppo.py
@@ -1,33 +1,37 @@
-import gymnasium as gym
-from custom_env import CounterEnv
+from tc2_env import TC2Env
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
 
 def train():
-    # env = gym.make("CartPole-v1", render_mode="rgb_array")
-    backing_env = CounterEnv(target=5, render_mode="human")
+    backing_env = TC2Env(render_mode="human")
     env = make_vec_env(lambda: backing_env, n_envs=1)
     print(env.observation_space)
     print(env.action_space)
 
     model = PPO("MlpPolicy", env, verbose=1, device="cpu")
-    model.learn(total_timesteps=5_000, log_interval=100)
+    model.learn(total_timesteps=100_000, log_interval=100)
     print("Training done")
 
-    model.save("ppo_counter")
-    del model
-    model = PPO.load("ppo_counter", device="cpu")
+    model.save("ppo_tc2_v2")
+
+
+def run():
+    model = PPO.load("ppo_tc2_v2", device="cpu")
     print("Model loaded")
 
-    # env = model.get_env()
+    backing_env = TC2Env(is_eval=True, render_mode="human")
+    env = make_vec_env(lambda: backing_env, n_envs=1)
     obs = env.reset()
     while True:
         action, _states = model.predict(obs, deterministic=True)
         obs, reward, terminated, info = env.step(action)
+        print(reward)
         env.render("human")
         if terminated:
+            print("Terminated")
             obs = env.reset()
 
 
-train()
+# train()
+run()
