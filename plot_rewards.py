4ef85267401cbb8899cb1f47770d22bb61f7a0e1
Shang Yi Ku <kushangyi2001@gmail.com>
2025-09-08 18:44:43 +0800

    Add reward plotting script

---
+++ plot_rewards.py
@@ -0,0 +1,17 @@
+from stable_baselines3.common.results_plotter import load_results, ts2xy
+import matplotlib.pyplot as plt
+
+
+version = "v4.0d"
+
+
+# Load Monitor logs
+data = load_results(f"./logs/{version}")
+timesteps, rewards = ts2xy(data, 'timesteps')
+
+# Plot reward progression
+plt.plot(timesteps, rewards)
+plt.xlabel("Timesteps")
+plt.ylabel("Episode Reward")
+plt.title("Training Reward Progress")
+plt.savefig(f"logs/{version}/training_rewards.png")
