9f5e3f7ac58c040c54a394192e9358bedda64b9c
Shang Yi Ku <kushangyi2001@gmail.com>
2025-09-20 21:37:01 +0800

    More work on Linux, MacOS sim bridge

--- game_bridge.py
+++ game_bridge.py
@@ -5,10 +5,8 @@ if os_name == "Windows":
     import mmap
     import win32event
     import struct
-elif os_name == "Darwin":
-    pass
-else:
-    pass
+elif os_name == "Darwin" or os_name == "Linux":
+    import posix_ipc
 
 
 from abc import ABC, abstractmethod
@@ -47,10 +45,10 @@ class GameBridge(ABC):
     def get_bridge_for_platform(cls, **kwargs):
         if os_name == "Windows":
             return WindowsGameBridge(**kwargs)
-        elif os_name == "Darwin":
-            raise NotImplementedError
+        elif os_name == "Darwin" or os_name == "Linux":
+            return UnixGameBridge(**kwargs)
         else:
-            raise NotImplementedError
+            raise NotImplementedError(f"Unknown OS {os_name}")
 
 
 class WindowsGameBridge(GameBridge):
@@ -92,3 +90,42 @@ class WindowsGameBridge(GameBridge):
         self.mm.seek(0)
         self.mm.write(struct.pack("bbbb", 1, hdg_action, alt_action, spd_action))
 
+
+class UnixGameBridge(GameBridge):
+    # 52 bytes shared region: [proceed flag(1 byte)] [action(3 bytes)] [reward(4 bytes (1 float))] [terminated(1 byte)] [empty padding(2 bytes)] [state(40 bytes (7x floats, 3x ints))]
+    FILE_SIZE = 52
+    STRUCT_FORMAT = "bbbbf?xxxfffffffiii"
+
+    def __init__(self, instance_suffix=""):
+        shm = posix_ipc.SharedMemory(f"ATCRLSharedMem{instance_suffix}", posix_ipc.O_CREX, size=self.__class__.FILE_SIZE)
+        self.mm = mmap.mmap(shm.fd, shm.size)
+        shm.close_fd()
+
+        self.reset_sim = posix_ipc.Semaphore(f"ATCRLResetEvent{instance_suffix}")
+        self.action_ready = posix_ipc.Semaphore(f"ATCRLActionReadyEvent{instance_suffix}")
+        self.action_done = posix_ipc.Semaphore(f"ATCRLActionDoneEvent{instance_suffix}")
+        self.reset_after_step = posix_ipc.Semaphore(f"ATCRLResetAfterEvent{instance_suffix}")
+
+    def signal_reset_sim(self):
+        self.reset_sim.release()
+
+    def wait_action_ready(self):
+        self.action_ready.acquire()
+
+    def signal_action_done(self):
+        self.action_done.release()
+
+    def signal_reset_after_step(self):
+        self.reset_after_step.release()
+
+    def get_total_state(self) -> tuple:
+        self.mm.seek(0)
+        return struct.unpack(self.__class__.STRUCT_FORMAT, self.mm.read(self.__class__.FILE_SIZE))
+
+    def get_aircraft_state(self) -> tuple:
+        self.mm.seek(12)
+        return struct.unpack(self.__class__.STRUCT_FORMAT[9:], self.mm.read(self.__class__.FILE_SIZE - 12))
+
+    def write_actions(self, hdg_action, alt_action, spd_action):
+        self.mm.seek(0)
+        self.mm.write(struct.pack("bbbb", 1, hdg_action, alt_action, spd_action))
